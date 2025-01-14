fcaf7cbfd71d177c0e20c6b7911aa6af6f1e0870
Asma <fathimasma26@gmail.com>
2025-01-14 10:21:32 +0530

    fixed port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 // Middleware
 app.use(express.json());
